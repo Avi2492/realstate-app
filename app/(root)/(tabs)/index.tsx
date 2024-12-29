49b9115830eb3fe688072f6725696b93abe21fe7
unknown <avinash24092@gmail.com>
2024-12-30 00:35:34 +0530

    Profile Page Completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,14 +1,38 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
 
 const index = () => {
 	return (
-		<View className="">
-			<Text className="font-bold text-3xl font-rubik my-10">
-				Welcome to Restate
-			</Text>
-		</View>
+		<SafeAreaView className="bg-white h-full">
+			<View className="px-5">
+				<View className="flex flex-row items-center justify-between mt-5">
+					<View className="flex flex-row items-center">
+						<Image
+							source={images.avatar}
+							className="size-12 rounded-full"
+						/>
+						<View className="flex flex-col items-start mt-2 justify-center">
+							<Text className="text-xs font-rubik text-black-100">
+								Good Morning
+							</Text>
+							<Text className="text-base font-rubik-medium text-black-300">
+								Avinash
+							</Text>
+						</View>
+					</View>
+					<Image
+						source={icons.bell}
+						className="size-6"
+					/>
+				</View>
+			</View>
+			<Search />
+		</SafeAreaView>
 	);
 };
 
