80c573b50bb8dee5b1a23896e87cb8fb5552791f
unknown <avinash24092@gmail.com>
2024-12-29 21:08:11 +0530

    Tabs & Signin

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -8,10 +8,6 @@ const index = () => {
 			<Text className="font-bold text-3xl font-rubik my-10">
 				Welcome to Restate
 			</Text>
-			<Link href={"/sign-in"}>Sign In</Link>
-			<Link href={"/explore"}>Explore</Link>
-			<Link href={"/profile"}>Profile</Link>
-			<Link href="/properties/1">Properties</Link>
 		</View>
 	);
 };
