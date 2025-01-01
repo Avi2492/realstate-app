88f3d84609e5358105f224c2b429b29ef01a72f3
unknown <avinash24092@gmail.com>
2025-01-01 12:40:12 +0530

    Home Page

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,6 +1,6 @@
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
-import { useLocalSearchParams, usePathname } from "expo-router";
+import { router, useLocalSearchParams, usePathname } from "expo-router";
 import icons from "@/constants/icons";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -10,10 +10,14 @@ const Search = () => {
 
 	const [search, setSearch] = useState(params.query);
 
-	const debouncedSearch = useDebouncedCallback();
+	const debouncedSearch = useDebouncedCallback(
+		(text: string) => router.setParams({ query: text }),
+		500,
+	);
 
 	const handleSearch = (text: string) => {
 		setSearch(text);
+		debouncedSearch(text);
 	};
 
 	return (
